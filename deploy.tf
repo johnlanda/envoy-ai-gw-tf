f52e1311b45c4f23b8e5b9aeac35e8ca4d98bde7
James Edward Bell Jr <13918870+abstract-base-method@users.noreply.github.com>
2025-04-21 21:06:19 -0500

    fix(tf):remove k8s in favor of postrender
    This was needed since the k8s provider can't configure without a cluster which is only created after first apply
    
    Signed-off-by: James Edward Bell Jr <13918870+abstract-base-method@users.noreply.github.com>

--- deploy.tf
+++ deploy.tf
@@ -11,55 +11,8 @@ resource "helm_release" "envoy_ai_gateway" {
   }
 
   depends_on = [module.eks]
-}
 
-resource "kubernetes_manifest" "gateway" {
-  depends_on = [data.aws_eks_cluster_auth.cluster, helm_release.envoy_ai_gateway]
-
-  manifest = {
-    apiVersion = "gateway.envoyproxy.io/v1alpha1"
-    kind = "EnvoyGateway"
-    metadata = {
-      name = "envoy-gateway"
-      namespace = "envoy-gateway-system"
-    }
-    spec = {
-      gateway = {
-        controllerName = "gateway.envoyproxy.io/gatewayclass-controller"
-      }
-      provider = {
-        type = "kubernetes"
-      }
-      logging = {
-        level = {
-          default = "info"
-        }
-      }
-      extensionApis = {
-        enable = true
-      }
-      rateLimit = {
-        backend = {
-          type = "Redis"
-          redis = {
-            url = "redis-service.envoy-gateway-system:6379"
-          }
-        }
-      }
-      extensionServer = {
-        port = 8080
-        host = "0.0.0.0"
-      }
-      extensionFilters = [
-        {
-          name = "bedrock"
-          type = "HTTP"
-          config = {
-            region = var.region
-            models = values(var.bedrock_models)
-          }
-        }
-      ]
-    }
+  postrender {
+    binary_path = "${path.root}/postrender-envoy-ai-gateway.sh"
   }
 }
--- terraform.tf
+++ terraform.tf
@@ -22,15 +22,9 @@ terraform {
 provider "aws" {
   region = var.region
 
-  default_tags = {
+  default_tags {
     tags = var.tags
   }
-}
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 provider "helm" {
